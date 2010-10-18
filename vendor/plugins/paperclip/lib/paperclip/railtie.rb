4fde5abfe913099313459977daa57f2384efa8db
Antoine Lenoir <khaz@khaz.info>
2010-10-18 14:44:22 +0200

    plop

--- vendor/plugins/paperclip/lib/paperclip/railtie.rb
+++ vendor/plugins/paperclip/lib/paperclip/railtie.rb
@@ -4,11 +4,11 @@ module Paperclip
   if defined? Rails::Railtie
     require 'rails'
     class Railtie < Rails::Railtie
-      initializer 'paperclip.insert_into_active_record' do
-        ActiveSupport.on_load :active_record do
-          Paperclip::Railtie.insert
-        end
-      end
+      # initializer 'paperclip.insert_into_active_record' do
+      #         ActiveSupport.on_load :active_record do
+      #           Paperclip::Railtie.insert
+      #         end
+      #       end
       rake_tasks do
         load "tasks/paperclip.rake"
       end
