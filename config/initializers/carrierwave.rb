bd740738aea9deab19279fd5429385aeda6eb136
Antoine Lenoir <khaz@khaz.info>
2010-10-20 14:14:12 +0200

    Heroku

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,5 @@
+CarrierWave.configure do |config|
+  config.cloud_files_username = 'khazou'
+  config.cloud_files_api_key = 'aed4dfcf78cb979587b6beacf940b2af'
+  config.cloud_files_container = 'Fenrir'
+end
